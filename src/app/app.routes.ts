be625f16f7d84b2eb0051d4b6648b7c2a1b478cf
Priyanka G S <priyaanju118@gmail.com>
2026-04-27 07:34:35 +0530

    bug fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  {
+    path: '',
+    loadComponent: () => import('./components/home').then(m => m.Home)
+  },
   {
     path: 'booking',
     loadComponent: () => import('./components/booking/booking').then(m => m.Booking)
